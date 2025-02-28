1cc04f5292f15d26215076a1da9b514f1a0061c8
rahulsingh-99 <rahul.rock.rr99@gmail.com>
2025-02-28 23:53:13 +0530

    Commit

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return <div>The requested page did not exist</div>;
+};
+
+export default NotFoundPage;
---
+++ app/users/new/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const NewUserPage = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <button onClick={() => router.push("/users")} className="btn btn-primary">
+        Create
+      </button>
+    </div>
+  );
+};
+
+export default NewUserPage;
